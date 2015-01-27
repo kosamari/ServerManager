39e373bf5be9213443bed174c85efd23a9160107
kosamari <mariko.kosaka@gmail.com>
2015-01-26 23:18:41 -0500

    adding nw interface

--- express.js
+++ express.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express();
-var port = 3000;
+// var port = 3000;
 var location = __dirname;
 
 app.use(express.static(location));
-app.listen(port);
+
+exports.run = function(port){
+    app.listen(port);
+}
+
+exports.close = function(){
+    app.close()
+}
